0baa931e8a84809ba188d61662a13e0904324c1d
dedovicnermin <dedovicnermin@outlook.com>
2021-04-07 08:57:13 -0500

    initial push

---
+++ src/main/java/tech/nermindedovic/kafkaconsumptionparallel/KafkaConsumptionParallelApplication.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.kafkaconsumptionparallel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumptionParallelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumptionParallelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/nermindedovic/kafkaconsumptionparallel/KafkaConsumptionParallelApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.kafkaconsumptionparallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumptionParallelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
